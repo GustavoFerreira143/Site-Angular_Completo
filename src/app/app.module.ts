3d9e8471073ffc7d5b70491dfc9dbd10ade04244
Gustavo <gustavo.feneita1@gmail.com>
2023-05-07 18:30:58 -0300

    Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
