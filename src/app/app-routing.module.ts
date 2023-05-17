db5a4f574bf36dcfb0104687aa27eea982f43955
Gustavo <gustavo.feneita1@gmail.com>
2023-05-17 19:42:21 -0300

    atividade_pronta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [{
-  path:'Inicio', component: InicioComponent},
-  {path:'Login', component: LoginComponent},
-  {path:'', redirectTo:'/inicio',pathMatch:'full'}
+import { RestritoComponent } from './restrito/restrito.component';
+import { GuardGuard } from './guard.guard';
+const routes: Routes = [
+{path: 'Inicio', component: InicioComponent},
+{path: 'Login', component: LoginComponent},
+{path: 'restrito',component: RestritoComponent, canActivate: [GuardGuard]},
+{path: '', redirectTo: '/Inicio', pathMatch:'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'Games LOLJA';
+  mostrarMenu:boolean= true;
+
+  constructor(private _loginService: LoginService){}
+  ngOnDestroy(): void {
+    localStorage.clear();
+    
+  }
+  ngOnInit(): void {
+   this._loginService.getmostaMenu().subscribe(
+    res => {this.mostrarMenu =res;});
+  }
+
 }
 
